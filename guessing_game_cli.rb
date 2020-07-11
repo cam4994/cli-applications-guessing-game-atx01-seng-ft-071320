6be2e1b9c1f36a83161ebd666648fcea4d21e292
Colin Mosley <colin.a.mosley@gmail.com>
2020-07-11 19:03:32 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   computer = rand(6) + 1
   puts "Please guess a number between 1 and 6"
   guess = gets.chomp
-  if random == guess 
+  if computer == guess 
     puts "You guessed the correct number!"
   elsif guess == "exit"
     puts "Goodbye!"
